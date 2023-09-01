94bde9286e47e04aca29fa9a3174d05b7c1ad75e
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-09-01 14:51:49 +0100

    Check connection state

---
+++ 0x03-queuing_system_in_js /0-redis_client.js
@@ -0,0 +1,13 @@
+// import redis from 'redis';
+import Redis from 'ioredis';
+const client = redis.createClient();
+
+const redisClient = new Redis();
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
